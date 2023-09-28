0188c289ad12b4a6368ec12b9538c71f67dc0a04
Grant Lemons <grant0lemons@gmail.com>
2023-09-27 18:32:09 -0600

    change to git repo version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[allow(unused_imports)]
+use smithay::{
+    reexports::{
+        calloop::{EventLoop, Interest, Mode, PostAction},
+        wayland_server::Display,
+    },
+    wayland::socket::ListeningSocketSource,
+};
+
 mod config;
 
 fn main() {
